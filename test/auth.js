20a50079657be6dd54398f69696b6067ac2febba
Wesley Li <adwhence@gmail.com>
2016-08-26 23:01:03 +1000

    fixed async test code

--- test/auth.js
+++ test/auth.js
@@ -2,11 +2,11 @@ var expect = require('expect.js');
 var auth = require('../auth');
 
 describe('authenticate', function() {
-    it('should return user with password hash', function () {
+    it('should return user with password hash', function (done) {
         auth.authenticate('user1', 'pass1', function (err, user) {
-            expect(err).to.not.be.ok();
             expect(user.username).to.be('user1');
             expect(user).to.have.property('password_hash');
+            done(err);
         });
     });
 });
