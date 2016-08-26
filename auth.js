56a19a15fa29c697389ae2ad469b06cc351961c1
Wesley Li <adwhence@gmail.com>
2016-08-26 22:26:44 +1000

    split auth

---
+++ auth.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('.config/adfs.json', { encoding: 'UTF8' }));
+
+exports.authenticate = function (username, password, done) {
+    cache.get(username, function (err, user) {
+        if (err) {
+            return done(err);
+        } else if (!user) {
+            return done(null, false);
+        } else if (user.password != password) {
+            return done(null, false);
+        } else {
+            return done(null, user);
+        }
+    });
+};
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 var express = require('express');
 var morgan = require('morgan');
-var fs = require('fs');
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
 var AWS = require('aws-sdk');
-
-var config = JSON.parse(fs.readFileSync('.config/main.json', { encoding: 'UTF8' }));
+var auth = require('./auth');
 
 var app = express();
 app.use(morgan('combined'));
+passport.use(new BasicStrategy(auth.authenticate));
 
-app.get(/^\/s3\/(.+?)\/(.+)$/, function (req, res) {
-    var bucket = req.params[0];
-    var key = req.params[1];
-    console.log('Piping from S3 Bucket: ' + bucket + ' / Key: ' + key + '\n');
-    var s3 = new AWS.S3();
-    var stream = s3.getObject({
-        Bucket: bucket,
-        Key: key
-    }).createReadStream();
-    stream.pipe(res);
-});
+app.get(/^\/s3\/(.+?)\/(.+)$/,
+    passport.authenticate('basic', { session: false }),
+    function (req, res) {
+        var bucket = req.params[0];
+        var key = req.params[1];
+        console.log('Piping from S3 Bucket: ' + bucket + ' / Key: ' + key + '\n');
+
+        var s3 = new AWS.S3();
+        var stream = s3.getObject({
+            Bucket: bucket,
+            Key: key
+        }).createReadStream();
+        stream.pipe(res);
+    }
+);
 
 var port = 3000;
 app.listen(port, function () {
