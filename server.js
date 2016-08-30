65de6d7f7ee651561ceaec4311166e2206f9de5c
Wesley Li <adwhence@gmail.com>
2016-08-30 21:46:39 +1000

    form login working

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+var flash = require('connect-flash');
 var AWS = require('aws-sdk');
 var fs = require('fs');
 var adfs = require('./adfs');
 
 var config = JSON.parse(fs.readFileSync('.config/adfs.json', { encoding: 'UTF8' }));
+
+// adfs
 
 var generateCredentials = function (username, password, done) {
     console.log('Fetching assertion from ADFS host');
@@ -24,6 +31,9 @@ var generateCredentials = function (username, password, done) {
         }
     });
 };
+
+
+// s3
 
 var pipeS3Stream = function (bucket, key, credentials, res, done) {
     var s3 = new AWS.S3({ credentials: new AWS.Credentials(
@@ -48,78 +58,76 @@ var pipeS3Stream = function (bucket, key, credentials, res, done) {
     });
 };
 
-var handleS3Request = function (req, res) {
-    var bucket = req.params[0];
-    var key = req.params[1];
 
-    if (req.session.credentials) {
-        pipeS3Stream(bucket, key, req.session.credentials, res, function (err) {
-            if (err) {
-                res.redirect('/login');
-            }
-        });
-    } else {
-        res.redirect('/login');
-    }
-};
+// passport
 
-var handleLogin = function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-
+passport.use(new LocalStrategy(function(username, password, done) {
     generateCredentials(username, password, function (err, credentials) {
         if (err) {
-            req.session.error = 'Authentication failed. ' + err;
-            res.redirect('/login');
+            return done(null, false, { message: err });
         } else {
-            req.session.regenerate(function (){
-                req.session.credentials = credentials;
-                res.redirect('back');
-            });
+            done(null, { username: username, credentials: credentials });
         }
     });
-};
+}));
 
-// configurate express
+passport.serializeUser(function (user, done) {
+    done(null, JSON.stringify(user));
+});
+
+passport.deserializeUser(function (json, done) {
+    done(null, JSON.parse(json));
+});
+
+
+
+// express
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
     secret: 'shhhh, very secret',
     cookie: { maxAge: 50 * 60 * 1000 } // 50 mins, slightly below the the adfs token expiry
 }));
-
-app.use(function(req, res, next){
-    var err = req.session.error;
-    var msg = req.session.success;
-    delete req.session.error;
-    delete req.session.success;
-    res.locals.message = '';
-    if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
-    if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
-    next();
-});
-
-app.get('/logout', function(req, res){
-    req.session.destroy(function(){
-        res.redirect('/');
-    });
-});
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/login', function(req, res){
-    res.render('login');
+    res.render('login', { message: req.flash('error') });
 });
 
-app.post('/login', handleLogin);
+app.post('/login',
+    passport.authenticate('local', {
+        successReturnToOrRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+);
 
 app.get(/^\/s3\/(.+?)\/(.+)$/,
-    handleS3Request
+    ensureLoggedIn('/login'),
+    function (req, res) {
+        var bucket = req.params[0];
+        var key = req.params[1];
+
+        if (req.user.credentials) {
+            pipeS3Stream(bucket, key, req.user.credentials, res, function (err) {
+                if (err) {
+                    res.redirect('/login');
+                }
+            });
+        } else {
+            res.redirect('/login');
+        }
+    }
 );
 
 var port = 3000;
