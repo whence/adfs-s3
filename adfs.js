88352bbd016af35ce3320c072c4368e091f0d1ce
Wesley Li <adwhence@gmail.com>
2016-09-03 21:58:49 +1000

    use airbnb style and async

--- adfs.js
+++ adfs.js
@@ -1,72 +1,76 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var AWS = require('aws-sdk');
+const request = require('request');
+const cheerio = require('cheerio');
+const AWS = require('aws-sdk');
 
-exports.isExpired = function (credentials, done) {
-    var iam = new AWS.IAM({ credentials: credentials });
-    iam.getAccountSummary(function (err) {
-        if (err) {
-            done(null, true);
-        } else {
-            done(null, false);
-        }
-    });
-};
+function isExpired(credentials, done) {
+  const iam = new AWS.IAM({ credentials });
+  iam.getAccountSummary((err) => {
+    if (err) {
+      done(null, true);
+    } else {
+      done(null, false);
+    }
+  });
+}
 
-exports.fetchAssertion = function (host, username, password, done) {
-    var url = 'https://' + host + '/adfs/ls/IdpInitiatedSignOn.aspx?loginToRp=urn:amazon:webservices';
-    var form = {
-        UserName: username,
-        Password: password,
-        AuthMethod: 'urn:amazon:webservices'
-    };
-    var jar = request.jar();
-    var options = {
-        url: url,
-        form: form,
-        jar: jar,
-        followAllRedirects: true
-    };
-    request.post(options, function (err, httpResponse, body) {
-        if (err) {
-            done(err);
-        } else if (httpResponse.statusCode !== 200) {
-            done('Request returns ' + httpResponse.statusCode);
+function fetchAssertion(host, username, password, done) {
+  const url = `https://${host}/adfs/ls/IdpInitiatedSignOn.aspx?loginToRp=urn:amazon:webservices`;
+  const form = {
+    UserName: username,
+    Password: password,
+    AuthMethod: 'urn:amazon:webservices'
+  };
+  const jar = request.jar();
+  const options = {
+    url,
+    form,
+    jar,
+    followAllRedirects: true
+  };
+  request.post(options, (err, httpResponse, body) => {
+    if (err) {
+      done(err);
+    } else if (httpResponse.statusCode !== 200) {
+      done(`Request returns ${httpResponse.statusCode}`);
+    } else {
+      const $ = cheerio.load(body);
+      const error = $('form#loginForm #errorText').text();
+      if (error) {
+        done(error);
+      } else {
+        const assertion = $('form[name=hiddenform] input[name=SAMLResponse]').prop('value');
+        if (assertion) {
+          done(null, assertion);
         } else {
-            var $ = cheerio.load(body);
-            var error = $('form#loginForm #errorText').text();
-            if (error) {
-                done(error);
-            } else {
-                var assertion = $('form[name=hiddenform] input[name=SAMLResponse]').prop('value');
-                if (assertion) {
-                    done(null, assertion);
-                } else {
-                    done('Empty SAMLResponse');
-                }
-            }
+          done('Empty SAMLResponse');
         }
-    });
-};
+      }
+    }
+  });
+}
 
-exports.obtainCredentials = function (roleArn, principalArn, assertion, done) {
-    var params = {
-        PrincipalArn: principalArn,
-        RoleArn: roleArn,
-        SAMLAssertion: assertion,
-        DurationSeconds: 3600
-    };
-    var sts = new AWS.STS();
-    sts.assumeRoleWithSAML(params, function (err, data) {
-        if (err) {
-            done(err);
-        } else {
-            var credentials = {
-                accessKeyId: data.Credentials.AccessKeyId,
-                secretAccessKey: data.Credentials.SecretAccessKey,
-                sessionToken: data.Credentials.SessionToken
-            };
-            done(null, credentials);
-        }
-    });
-};
+function obtainCredentials(roleArn, principalArn, assertion, done) {
+  const params = {
+    PrincipalArn: principalArn,
+    RoleArn: roleArn,
+    SAMLAssertion: assertion,
+    DurationSeconds: 3600
+  };
+  const sts = new AWS.STS();
+  sts.assumeRoleWithSAML(params, (err, data) => {
+    if (err) {
+      done(err);
+    } else {
+      const credentials = {
+        accessKeyId: data.Credentials.AccessKeyId,
+        secretAccessKey: data.Credentials.SecretAccessKey,
+        sessionToken: data.Credentials.SessionToken
+      };
+      done(null, credentials);
+    }
+  });
+}
+
+exports.isExpired = isExpired;
+exports.fetchAssertion = fetchAssertion;
+exports.obtainCredentials = obtainCredentials;
--- server.js
+++ server.js
@@ -1,141 +1,136 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
-var flash = require('connect-flash');
-var AWS = require('aws-sdk');
-var fs = require('fs');
-var debug = require('debug');
-var adfs = require('./adfs');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+const flash = require('connect-flash');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const debug = require('debug');
+const async = require('async');
+const adfs = require('./adfs');
 
-var config = JSON.parse(fs.readFileSync('.config/adfs.json', { encoding: 'UTF8' }));
+const config = JSON.parse(fs.readFileSync('.config/adfs.json', { encoding: 'UTF8' }));
 
 // adfs
 
-var debugAdfs = debug('adfs');
-var generateCredentials = function (username, password, done) {
-    debugAdfs('%s fetching assertion from ADFS host', username);
-    adfs.fetchAssertion(config.host, username, password, function (err, assertion) {
-        if (err) {
-            done(err);
-        } else {
-            debugAdfs('%s obtaining AWS credentials from assertion', username);
-            adfs.obtainCredentials(config.roleArn, config.principalArn, assertion, function (err, credentials) {
-                if (err) {
-                    done(err);
-                } else {
-                    done(null, credentials);
-                }
-            });
-        }
-    });
-};
+const debugAdfs = debug('adfs');
+function generateCredentials(username, password, done) {
+  async.waterfall([
+    (cb) => {
+      debugAdfs('%s fetching assertion from ADFS host', username);
+      adfs.fetchAssertion(config.host, username, password, cb);
+    },
+    (assertion, cb) => {
+      debugAdfs('%s obtaining AWS credentials from assertion', username);
+      adfs.obtainCredentials(config.roleArn, config.principalArn, assertion, cb);
+    }
+  ], done);
+}
 
 
 // s3
 
-var debugS3 = debug('s3');
-var pipeS3Stream = function (bucket, key, req, res, done) {
-    var s3 = new AWS.S3({ credentials: new AWS.Credentials(
-        req.user.credentials.accessKeyId,
-        req.user.credentials.secretAccessKey,
-        req.user.credentials.sessionToken
-    )});
-    var request = s3.getObject({
-        Bucket: bucket,
-        Key: key
-    });
-    var stream = request.createReadStream();
-    stream.pipe(res);
+const debugS3 = debug('s3');
+function pipeS3Stream(bucket, key, req, res, done) {
+  const s3 = new AWS.S3({ credentials: new AWS.Credentials(
+    req.user.credentials.accessKeyId,
+    req.user.credentials.secretAccessKey,
+    req.user.credentials.sessionToken
+  ) });
+  const request = s3.getObject({
+    Bucket: bucket,
+    Key: key
+  });
+  const stream = request.createReadStream();
+  stream.pipe(res);
 
-    request.on('success', function () {
-        debugS3('%s piped from s3://%s/%s', req.user.username, bucket, key);
-        done(null);
-    });
-    stream.on('error', function () {
-        debugS3('%s failed to obtain S3 stream', req.user.username);
-        done('Failed to pipe S3 stream');
-    });
-};
+  request.on('success', () => {
+    debugS3('%s piped from s3://%s/%s', req.user.username, bucket, key);
+    done(null);
+  });
+  stream.on('error', () => {
+    debugS3('%s failed to obtain S3 stream', req.user.username);
+    done('Failed to pipe S3 stream');
+  });
+}
 
 
 // passport
 
-var debugPassport = debug('passport');
+const debugPassport = debug('passport');
 
-passport.use(new LocalStrategy(function(username, password, done) {
-    generateCredentials(username, password, function (err, credentials) {
-        if (err) {
-            debugPassport('%s login failed: %s', username, err);
-            return done(null, false, { message: err });
-        } else {
-            debugPassport('%s login succeed', username);
-            done(null, { username: username, credentials: credentials });
-        }
-    });
+passport.use(new LocalStrategy((username, password, done) => {
+  generateCredentials(username, password, (err, credentials) => {
+    if (err) {
+      debugPassport('%s login failed: %s', username, err);
+      done(null, false, { message: err });
+    } else {
+      debugPassport('%s login succeed', username);
+      done(null, { username, credentials });
+    }
+  });
 }));
 
-passport.serializeUser(function (user, done) {
-    done(null, JSON.stringify(user));
+passport.serializeUser((user, done) => {
+  done(null, JSON.stringify(user));
 });
 
-passport.deserializeUser(function (json, done) {
-    done(null, JSON.parse(json));
+passport.deserializeUser((json, done) => {
+  done(null, JSON.parse(json));
 });
-
 
 
 // express
 
-var debugExpress = debug('express');
+const debugExpress = debug('express');
 
-var app = express();
+const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: 'shhhh, very secret',
-    cookie: { maxAge: 50 * 60 * 1000 } // 50 mins, slightly below the the adfs token expiry
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret',
+  cookie: { maxAge: 50 * 60 * 1000 } // 50 mins, slightly below the the adfs token expiry
 }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/login', function(req, res){
-    res.render('login', { message: req.flash('error') });
+app.get('/login', (req, res) => {
+  res.render('login', { message: req.flash('error') });
 });
 
 app.post('/login',
-    passport.authenticate('local', {
-        successReturnToOrRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
+  passport.authenticate('local', {
+    successReturnToOrRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
 );
 
 app.get(/^\/s3\/(.+?)\/(.+)$/,
     ensureLoggedIn('/login'),
-    function (req, res) {
-        var bucket = req.params[0];
-        var key = req.params[1];
+    (req, res) => {
+      const bucket = req.params[0];
+      const key = req.params[1];
 
-        pipeS3Stream(bucket, key, req, res, function (err) {
-            if (err) {
-                res.redirect('/login');
-            }
-        });
+      pipeS3Stream(bucket, key, req, res, (err) => {
+        if (err) {
+          res.redirect('/login');
+        }
+      });
     }
 );
 
-var port = 3000;
-app.listen(port, function () {
-    debugExpress('Server running on localhost:' + port);
+const port = 3000;
+app.listen(port, () => {
+  debugExpress(`Server running on localhost:${port}'`);
 });
